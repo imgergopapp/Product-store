fd9377d2e7d1ae53214a22f32d6dd2b3fe673c8d
Gergő Papp <imgergopapp@gmail.com>
2019-06-06 17:20:54 +0200

    Implemented close() method in AbstractDao

--- src/main/java/com/codecool/web/dao/database/AbstractDao.java
+++ src/main/java/com/codecool/web/dao/database/AbstractDao.java
@@ -1,11 +1,16 @@
 package com.codecool.web.dao.database;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 abstract class AbstractDao implements AutoCloseable {
     protected final Connection connection;
 
     public AbstractDao(Connection connection) {
         this.connection = connection;
+    }
+
+    public void close() throws SQLException {
+        connection.close();
     }
 }
