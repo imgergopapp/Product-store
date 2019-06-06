36928f1a7f52661a16e52535107b8e03de14b630
Gergő Papp <imgergopapp@gmail.com>
2019-06-06 18:35:02 +0200

    Implemented executeInsert method

--- src/main/java/com/codecool/web/dao/database/AbstractDao.java
+++ src/main/java/com/codecool/web/dao/database/AbstractDao.java
@@ -1,6 +1,7 @@
 package com.codecool.web.dao.database;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 abstract class AbstractDao implements AutoCloseable {
@@ -8,6 +9,14 @@ abstract class AbstractDao implements AutoCloseable {
 
     public AbstractDao(Connection connection) {
         this.connection = connection;
+    }
+
+    void executeInsert(PreparedStatement statement) throws SQLException {
+        int insertCount = statement.executeUpdate();
+        if (insertCount != 1) {
+            connection.rollback();
+            throw new SQLException("Expected 1 row to be inserted");
+        }
     }
 
     public void close() throws SQLException {
