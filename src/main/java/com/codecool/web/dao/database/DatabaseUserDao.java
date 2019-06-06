46a8f40c0b4d9302925094eb49307c514b3c1f12
Gergő Papp <imgergopapp@gmail.com>
2019-06-06 18:06:57 +0200

    Implemented findById (User) method

--- src/main/java/com/codecool/web/dao/database/DatabaseUserDao.java
+++ src/main/java/com/codecool/web/dao/database/DatabaseUserDao.java
@@ -19,8 +19,15 @@ public class DatabaseUserDao extends AbstractDao implements UserDao {
     }
 
     @Override
-    public User findById(int id) throws SQLException {
-        return null;
+    public User findById(int id) throws SQLException, DaoParserException {
+        String sql = "SELECT user_id, user_name, email, role, country, zip_code, city, street FROM users WHERE user_id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+            statement.execute();
+            ResultSet resultSet = statement.getResultSet();
+            resultSet.next();
+            return fetchUser(resultSet);
+        }
     }
 
     @Override
@@ -51,5 +58,4 @@ public class DatabaseUserDao extends AbstractDao implements UserDao {
 
         return new User(id, name, email, role, new Address(country, zip_code, city, street));
     }
-}
 }
