de3b99d1c02c7d923c2bd3f8f2186f6c8bebcdbf
Gergő Papp <imgergopapp@gmail.com>
2019-06-06 15:30:59 +0200

    Implemented init.js (onLoad method)

---
+++ src/main/webapp/init.js
@@ -0,0 +1,14 @@
+function onLoad() {
+    loginContentDivEl = document.getElementById('login-content');
+    registerContentDivEl = document.getElementById('register-content');
+    logoutContentDivEl = document.getElementById('logout-content');
+
+    const loginButtonEl = document.getElementById('login-button');
+    loginButtonEl.addEventListener('click', onLoginButtonClicked);
+
+    const registerButtonEl = document.getElementById('register-button');
+    registerButtonEl.addEventListener('click', onRegisterButtonClicked);
+
+    const backToLoginButtonEl = document.getElementById('back-to-login-button');
+    backToLoginButtonEl.addEventListener('click', onBackToLoginButtonClicked);
+}
