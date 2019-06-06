54d6275d42b7d6acb869fd66f265f4eb81e322d8
Gergő Papp <imgergopapp@gmail.com>
2019-06-06 20:15:48 +0200

    Implemented SimpleUserService class

---
+++ src/main/java/com/codecool/web/service/simple/SimpleUserService.java
@@ -0,0 +1,37 @@
+package com.codecool.web.service.simple;
+
+import com.codecool.web.dao.UserDao;
+import com.codecool.web.model.Address;
+import com.codecool.web.model.User;
+import com.codecool.web.service.UserService;
+
+import java.sql.SQLException;
+
+public class SimpleUserService implements UserService {
+
+    private final UserDao userDao;
+
+    public SimpleUserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public User findById(int id) throws SQLException {
+        return userDao.findById(id);
+    }
+
+    @Override
+    public User findByEmail(String email) throws SQLException {
+        return userDao.findByEmail(email);
+    }
+
+    @Override
+    public void registerUser(String name, String password, String email, String role, Address address) throws SQLException {
+        userDao.registerUser(name,password,email,role,address);
+    }
+
+    @Override
+    public boolean isRegistered(String email, String password) throws SQLException {
+        return userDao.isRegistered(email,password);
+    }
+}
