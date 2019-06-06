99d4643e53590d022bb80e7eb9526e7f4647e475
Gergő Papp <imgergopapp@gmail.com>
2019-06-06 19:55:11 +0200

    Removed isEmailAvailable method from interfaces (private visibility)

--- src/main/java/com/codecool/web/dao/UserDao.java
+++ src/main/java/com/codecool/web/dao/UserDao.java
@@ -9,6 +9,5 @@ import java.sql.SQLException;
 public interface UserDao {
     User findById(int id) throws SQLException, DaoParserException;
     void registerUser(String name,String password, String email, String role, Address address) throws SQLException;
-    boolean isEmailAvailable(String email) throws SQLException;
     boolean isRegistered(String email, String password) throws SQLException;
 }
