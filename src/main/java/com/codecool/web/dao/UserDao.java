94465752149ea02d61fb94a51638de1261e1ea29
Gergő Papp <imgergopapp@gmail.com>
2019-06-06 18:08:20 +0200

    Added DaoParserException to UserDao interface

--- src/main/java/com/codecool/web/dao/UserDao.java
+++ src/main/java/com/codecool/web/dao/UserDao.java
@@ -1,12 +1,13 @@
 package com.codecool.web.dao;
 
+import com.codecool.web.dao.parser.DaoParserException;
 import com.codecool.web.model.Address;
 import com.codecool.web.model.User;
 
 import java.sql.SQLException;
 
 public interface UserDao {
-    User findById(int id) throws SQLException;
+    User findById(int id) throws SQLException, DaoParserException;
     void registerUser(String name, String email, Address address) throws SQLException;// role is default : REGULAR
     boolean isEmailAvailable(String email) throws SQLException;
     boolean isRegistered(String email, String password) throws SQLException;
