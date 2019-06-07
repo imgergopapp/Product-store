dc0768466fe99e5a6af20fde8b54ea01bb410313
Gergő Papp <imgergopapp@gmail.com>
2019-06-07 11:31:05 +0200

    Changed LogoutServlet's doPost HTTP method

--- src/main/java/com/codecool/web/servlet/LoginServlet.java
+++ src/main/java/com/codecool/web/servlet/LoginServlet.java
@@ -2,10 +2,10 @@ package com.codecool.web.servlet;
 
 import com.codecool.web.dao.UserDao;
 import com.codecool.web.dao.database.DatabaseUserDao;
+import com.codecool.web.model.User;
 import com.codecool.web.service.UserService;
 import com.codecool.web.service.simple.SimpleUserService;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,10 +14,10 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 @WebServlet("/login")
-public class LoginServlet extends AbstractServlet{
+public class LoginServlet extends AbstractServlet {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         try (Connection connection = getConnection(req.getServletContext())) {
             UserDao userDao = new DatabaseUserDao(connection);
             UserService userService = new SimpleUserService(userDao);
@@ -25,10 +25,12 @@ public class LoginServlet extends AbstractServlet{
             String email = req.getParameter("email");
             String password = req.getParameter("password");
 
-            userService.isRegistered(email,password);
-            req.setAttribute("user",userService.findByEmail(email));
+            userService.isRegistered(email, password);
+            User user = userService.findByEmail(email);
+            req.getSession().setAttribute("user", user);
+            sendMessage(resp, HttpServletResponse.SC_OK, user);
         } catch (SQLException se) {
-            handleSqlError(resp,se);
+            handleSqlError(resp, se, false);
         }
     }
 }
