9c94fccd4f4cc0903992ab2349ffe834bc5700b8
Gergő Papp <imgergopapp@gmail.com>
2019-06-11 20:40:34 +0200

    Renamed Address model's zip_code field to zipCode

--- src/main/java/com/codecool/web/model/Address.java
+++ src/main/java/com/codecool/web/model/Address.java
@@ -2,13 +2,13 @@ package com.codecool.web.model;
 
 public class Address {
     private String country;
-    private String zip_code;
+    private String zipCode;
     private String city;
     private String street;
 
-    public Address(String country, String zip_code, String city, String street) {
+    public Address(String country, String zipCode, String city, String street) {
         this.country = country;
-        this.zip_code = zip_code;
+        this.zipCode = zipCode;
         this.city = city;
         this.street = street;
     }
@@ -17,8 +17,8 @@ public class Address {
         return country;
     }
 
-    public String getZip_code() {
-        return zip_code;
+    public String getZipCode() {
+        return zipCode;
     }
 
     public String getCity() {
