7c7238408809bfd1ad739944befb5e4332abb31f
Gergő Papp <imgergopapp@gmail.com>
2019-06-11 21:00:35 +0200

    Changed updateProfile & added isProfileChanged method

--- src/main/java/com/codecool/web/service/simple/SimpleUserService.java
+++ src/main/java/com/codecool/web/service/simple/SimpleUserService.java
@@ -4,6 +4,7 @@ import com.codecool.web.dao.UserDao;
 import com.codecool.web.model.Address;
 import com.codecool.web.model.User;
 import com.codecool.web.service.UserService;
+import com.codecool.web.service.exception.ServiceException;
 
 import java.sql.SQLException;
 
@@ -29,4 +30,23 @@ public class SimpleUserService implements UserService {
     public void registerUser(String name, String password, String email) throws SQLException {
         userDao.registerUser(name,password,email);
     }
+
+    @Override
+    public User updateProfile(String name, String email, Address address) throws ServiceException, SQLException {
+        if (isProfileChanged(null,null,null)){
+            userDao.updateProfile(name, email, address);
+            return findByEmail(email);
+        }
+        else {
+            throw new ServiceException("Nothing saved! (No changes)");
+        }
+
+    }
+
+    private boolean isProfileChanged(User user, String newName, Address newAddres){
+       // if ()
+        return true; // TODO : implement
+    }
+
+
 }
