51c4dddf40e8c760a5a1200d28d5c2a8fe137281
Gergő Papp <imgergopapp@gmail.com>
2019-06-11 20:00:05 +0200

    Added profile (div & buttons) to init.js

--- src/main/webapp/init.js
+++ src/main/webapp/init.js
@@ -2,10 +2,14 @@ function onLoad() {
     loginContentDivEl = document.getElementById('login-content');
     registerContentDivEl = document.getElementById('register-content');
     logoutContentDivEl = document.getElementById('logout-content');
+    welcomeContentDivEl = document.getElementById('welcome-content');
+    profileContentDivEl = document.getElementById('profile-content');
 
+    // Login
     const loginButtonEl = document.getElementById('login-button');
     loginButtonEl.addEventListener('click', onLoginButtonClicked);
 
+    // Registration
     const registerButtonEl = document.getElementById('register-button');
     registerButtonEl.addEventListener('click', onRegisterButtonClicked);
 
@@ -14,6 +18,16 @@ function onLoad() {
 
     const backToLoginButtonEl = document.getElementById('back-to-login-button');
     backToLoginButtonEl.addEventListener('click', backToLogin);
+
+    // Profile
+    const cancelProfileEditButtonEl = document.getElementById('cancel-profile-edit-button');
+    cancelProfileEditButtonEl.addEventListener('click', onCancelProfileEditClicked);
+
+    const onProfileUpdateButtonEl = document.getElementById('update-profile-button');
+    onProfileUpdateButtonEl.addEventListener('click', onProfileUpdateClicked);
+
+    
+
 }
 
 document.addEventListener('DOMContentLoaded', onLoad);
