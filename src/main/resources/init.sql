bc1dc434c74583d5a8d015fad3336f0997fbbfcb
Gergő Papp <imgergopapp@gmail.com>
2019-06-12 12:14:04 +0200

    Removed sale table (simplified & merged to products)

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -8,7 +8,6 @@ DROP FUNCTION IF EXISTS order_completion_date_check;
 
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS products CASCADE;
-DROP TABLE IF EXISTS sale CASCADE;
 DROP TABLE IF EXISTS carts CASCADE;
 DROP TABLE IF EXISTS transporters CASCADE;
 DROP TABLE IF EXISTS orders CASCADE;
@@ -34,16 +33,8 @@ CREATE TABLE products(
 	product_company VARCHAR(40),
 	product_price INTEGER,
 	in_stock INTEGER,
-	is_on_sale BOOLEAN,
+	sale_percentage NUMERIC(2) DEFAULT 0,
 	picture_url VARCHAR(100)
-);
-
-CREATE TABLE sale(
-    product_id INTEGER,
-    sale_percent NUMERIC(2),
-    date_from DATE,
-    date_until DATE
-
 );
 
 CREATE TABLE carts(
@@ -79,9 +70,6 @@ ALTER TABLE carts
 ALTER TABLE orders
 	ADD CONSTRAINT orders_fk_cart_id FOREIGN KEY (cart_id) REFERENCES carts(cart_id),
 	ADD CONSTRAINT orders_fk_transported_id FOREIGN KEY (transporter_id) REFERENCES transporters(transporter_id);
-
-ALTER TABLE sale
-	ADD CONSTRAINT sale_fk_product_id FOREIGN KEY (product_id) REFERENCES products(product_id);
 
 CREATE FUNCTION products_in_stock_check() RETURNS trigger
 AS '
@@ -127,15 +115,12 @@ INSERT INTO users (user_name, password, email, role, country, zip_code, city, st
 	('user2', crypt('pw2', gen_salt('bf', 9)), 'user2@user2.com', 'RETAILER', 'Hungary', 5000,'cityb','Cool street, 18'), --2
 	('user1', crypt('pw2', gen_salt('bf', 9)), 'user1@user1.com', 'ADMIN', 'Hungary', 6000,'cityb','Cooler street, 81'); --3
 
-INSERT INTO products (product_name, main_category, sub_category, properties, product_company, product_price, in_stock, is_on_sale, picture_url) VALUES
-    ('z-301 ', 'it', 'processor', 'small;fast', 'That company', 1000, 10, true,'https://cdn.pixabay.com/photo/2019/05/31/16/01/cpu-4242470_1280.jpg'), --1
-    ('qh-3 ', 'it', 'processor', 'ultra-small;ultra-fast', 'This company', 2000, 5, false, 'https://cdn.pixabay.com/photo/2017/04/10/07/57/processor-2217771_1280.jpg'), --2
-    ('mxm-2', 'it', 'processor', 'nano;hyper-fast', 'That company', 2500, 10, false, 'https://cdn.pixabay.com/photo/2017/12/09/11/16/electronics-3007664_1280.jpg'), --3
-    ('srs2000', 'it', 'hdd', 'very-hard disk drive; very fast', 'One man company', 500, 11, false, 'https://cdn.pixabay.com/photo/2013/07/12/19/16/hdd-154463_1280.png'), --4
-    ('qqq-111', 'sport', 'bike', 'medium;lightweight;21 speed', 'Random bycicle company', 15000, 2, false, 'https://cdn.pixabay.com/photo/2013/07/13/13/46/bicycle-161524_1280.png'); --5
-
-INSERT INTO sale (product_id, sale_percent, date_from, date_until) VALUES
-    (1,10,'2019-01-01','2019-02-01');
+INSERT INTO products (product_name, main_category, sub_category, properties, product_company, product_price, in_stock, sale_percentage, picture_url) VALUES
+    ('z-301 ', 'it', 'processor', 'small;fast', 'That company', 1000, 10, 10,'https://cdn.pixabay.com/photo/2019/05/31/16/01/cpu-4242470_1280.jpg'), --1
+    ('qh-3 ', 'it', 'processor', 'ultra-small;ultra-fast', 'This company', 2000, 5, 20, 'https://cdn.pixabay.com/photo/2017/04/10/07/57/processor-2217771_1280.jpg'), --2
+    ('mxm-2', 'it', 'processor', 'nano;hyper-fast', 'That company', 2500, 10, 0, 'https://cdn.pixabay.com/photo/2017/12/09/11/16/electronics-3007664_1280.jpg'), --3
+    ('srs2000', 'it', 'hdd', 'very-hard disk drive; very fast', 'One man company', 500, 11, 0, 'https://cdn.pixabay.com/photo/2013/07/12/19/16/hdd-154463_1280.png'), --4
+    ('qqq-111', 'sport', 'bike', 'medium;lightweight;21 speed', 'Random bycicle company', 15000, 2, 0, 'https://cdn.pixabay.com/photo/2013/07/13/13/46/bicycle-161524_1280.png'); --5
 
 INSERT INTO carts (product_id, user_id, quantity, products_price) VALUES
 	(1, 1, 3, 4500), --1
